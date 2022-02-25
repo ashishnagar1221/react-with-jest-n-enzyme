f76f4103241a56b56c42b0fe75b4182b4812755b
Ashish Nagar <ashish_nagar@mitiztechnologies.com>
2022-02-26 01:49:13 +0530

    test cases and expected output

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,31 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from "./App";
+import { shallow } from "enzyme";
+import PersonList from "./PersonList";
+describe("App", () => {
+  let appWrapper;
+  beforeAll(() => {
+    appWrapper = shallow(<App />);
+  });
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  it("render without crashing", () => {});
+
+  it("render a person list  ", () => {
+    const personList = appWrapper.find(PersonList);
+    expect(personList).toHaveLength(1);
+  });
+
+  it("has state", () => {
+    const appState = appWrapper.state();
+    expect(appState).not.toBeNull();
+  });
+
+  it("has a people property on state", () => {
+    const appState = appWrapper.state();
+    expect(appState.people).toBeDefined();
+  });
+
+  it("passes prople property state to personList as props", () => {
+    const personList = appWrapper.find(PersonList);
+    expect(personList.props().people).toEqual(appWrapper.state().people);
+  });
 });
